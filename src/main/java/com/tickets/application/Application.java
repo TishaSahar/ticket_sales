afd05d40251a3995158fad687b33924a47830828
TikhonSakharov <andreev.tihon@gmail.com>
2024-02-13 11:55:14 +0300

    test commit

--- src/main/java/com/tickets/application/Application.java
+++ src/main/java/com/tickets/application/Application.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(Application.class, args);
         log.info("Application run with args: ", args.toString());
     }
