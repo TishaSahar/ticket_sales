cefeb57ccccd8b8687c0e4fb77a9c2512bc492f6
TikhonSakharov <andreev.tihon@gmail.com>
2024-02-20 01:43:05 +0300

    Moved all settings to the one .yaml file. Implemented script for server running.

---
+++ run_local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run docker containeer with redis and postgresql volumes
+docker-compose -f docker-compose.yaml up -d --build
+
+# Run apring boot applicaton from target directory
+ScriptDir=$(pwd)
+cd $ScriptDir/target
+java -jar app-3.1.3.jar
+cd $ScriptDir
+
+
