79643b0df3e0a3165e52b7d11528eb93e4385527
TikhonSakharov <andreev.tihon@gmail.com>
2024-02-21 21:07:54 +0300

    Redisson cache: implemented configuration

--- src/main/java/com/tickets/application/Application.java
+++ src/main/java/com/tickets/application/Application.java
@@ -1,10 +1,20 @@
 package com.tickets.application;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Main spring boot application.
@@ -16,5 +26,21 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("Application run with args: ", args.toString());
+    }
+
+    @Bean(destroyMethod="shutdown")
+    RedissonClient redisson() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379");
+        return Redisson.create(config);
+    }
+    @Bean
+    CacheManager cacheManager(RedissonClient redissonClient) {
+        Map<String, CacheConfig> config = new HashMap<>();
+
+        // create "testMap" spring cache with ttl = 24 minutes and maxIdleTime = 12 minutes
+        config.put("testMap", new CacheConfig(24*60*1000, 12*60*1000));
+        return new RedissonSpringCacheManager(redissonClient, config);
     }
 }
